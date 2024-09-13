868ea5a6c20bb2cbb24173f9560817fbbe51c8e6
Silvio Cardo <slv.cardo@gmail.com>
2024-09-13 23:37:13 +0200

    solved first TODO

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // TODO
-// 1 - For the current move only, show “You are at move #…” instead of a button.
+// [done] 1 - For the current move only, show “You are at move #…” instead of a button.
 // 2 - Rewrite Board to use two loops to make the squares instead of hardcoding them.
 // 3 - Add a toggle button that lets you sort the moves in either ascending or descending order.
 // 4 - When someone wins, highlight the three squares that caused the win (and when no one wins, display a message about the result being a draw).
@@ -98,10 +98,12 @@ export default function Game(){
   const moves = history.map(
     (squares, move) => {
       let description
-      if (move > 0) {
+      if (move === 0) {
+        description = "Go to game start"
+      } else if (currentMove != move) {
         description = "Go to move #" + move
       } else {
-        description = "Go to game start"
+        description = "You are at move #" + move
       }
       return (
         <li key={move}>
